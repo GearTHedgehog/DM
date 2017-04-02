143142bcf10e658e0de0ee497815fd13c00f21a8
GearTHedgehog <danya400@yandex.ru>
2017-04-02 18:24:21 +0300

    Update DMmodP.h

--- DMmodP.h
+++ DMmodP.h
@@ -3,7 +3,7 @@
 #include <C:\Users\Extranet\Desktop\DM_6305_KL-master\gear.h>
 using namespace std;
 
-//собственные функции, разработанные для поддержания работы нескольких других функций
+//Г±Г®ГЎГ±ГІГўГҐГ­Г­Г»ГҐ ГґГіГ­ГЄГ¶ГЁГЁ, Г°Г Г§Г°Г ГЎГ®ГІГ Г­Г­Г»ГҐ Г¤Г«Гї ГЇГ®Г¤Г¤ГҐГ°Г¦Г Г­ГЁГї Г°Г ГЎГ®ГІГ» Г­ГҐГ±ГЄГ®Г«ГјГЄГЁГµ Г¤Г°ГіГЈГЁГµ ГґГіГ­ГЄГ¶ГЁГ©
 t_Rat POWER_Q(t_Rat, int)
 int COM_QQ_I(t_Rat, t_Rat)
 int COM_PP_I(t_Poli, t_Poli)
@@ -27,9 +27,9 @@ t_Poli ADD_PP_P(t_Poli p1, t_Poli p2)
 		p1.koefP = realloc(p1.koefP, sizeof(t_Rat) * (p2.m + 1));
 		for (j = p1.m + 1; j <= p2.m; j++)
 		{
-			for (i = p1.koefP[j].numerator.num; i >= 0; i--)
+			for (i = p1.koefP[j].Dgr_Z; i >= 0; i--)
 			{
-				p1.koefP[j].numerator.koef[i] = 0;
+				p1.koefP[j].NumerZ[i] = 0;
 			}
 		}
 		for (i = 0; i <= p2.m; i++)
@@ -61,9 +61,9 @@ t_Poli SUB_PP_P(t_Poli p1, t_Poli p2)
 		p1.koefP = realloc(p1.koefP, sizeof(t_Rat)*(p2.m + 1));
 		for (j = p1.m + 1; j <= p2.m; j++)
 		{
-			for (i = p1.koefP[j].numerator.num; i >= 0; i--)
+			for (i = p1.koefP[j].Dgr_Z; i >= 0; i--)
 			{
-				p1.koefP[j].numerator.koef[i] = 0;
+				p1.koefP[j].Numer_Z[i] = 0;
 			}
 		}
 		for (i = p2.m; i >= 0; i--)
@@ -92,9 +92,9 @@ t_Poli MUL_Pxk_P(t_Poli p, int xk)
 	result.koefP = malloc(sizeof(t_Rat) * (result.m + 1));
 	for (j = 0; j <= p.m; j++)
 	{
-		for (i = p.koefP[j].numerator.num; i >= 0; i--)
+		for (i = p.koefP[j].Dgr_Z; i >= 0; i--)
 		{
-			result.koefP[j].numerator.koef[i] = 0;
+			result.koefP[j].Numer_Z[i] = 0;
 		}
 	}
 	for (i = 0; i <= result.m; i++, j++)
@@ -120,18 +120,18 @@ t_Rat FAC_P_Q(t_Poli p)
 	t_Rat result;
 	t_Nat tmpGCF;
 	t_Nat tmpLCM;
-	tmpGCF = TRANS_Z_N(ABS_Z_N(p.koefP[0].numerator));
+	tmpGCF = TRANS_Z_N(ABS_Z_N(p.koefP[0].Numer_Z));
 	for (i = 1; i < p.m; i++)
 	{
-		tmpGCF = GCF_NN_N(tmpGCF, TRANS_Z_N(ABS_Z_N(p.koefP[i].numerator)));
+		tmpGCF = GCF_NN_N(tmpGCF, TRANS_Z_N(ABS_Z_N(p.koefP[i].Numer_Z)));
 	}
-	tmpLCM = p.koefP[0].denominator;
+	tmpLCM = p.koefP[0].Denom_N;
 	for (i = 1; i < p.m; i++)
 	{
-		tmpLCM = LCM_NN_N(tmpLCM, p.koefP[i].denominator);
+		tmpLCM = LCM_NN_N(tmpLCM, p.koefP[i].Denom_N);
 	}
-	result.numerator = TRANS_N_Z(tmpGCF);
-	result.denominator = tmpLCM;
+	result.Numer_Z = TRANS_N_Z(tmpGCF);
+	result.Denom_N = tmpLCM;
 	return result;
 }
 
@@ -143,7 +143,7 @@ t_Poli MUL_PP_P(t_Poli p1, t_Poli p2)
 	result.koefP = malloc(result.m + 1);
 	for (i = 0; i <= p1.m; i++)
 	{
-		if (p1.koefP[i].numerator.koef[p1.koefP[i].numerator.num] != 0)
+		if (p1.koefP[i].Numer_Z[p1.koefP[i].Dgr_Z] != 0)
 		{
 			result = ADD_PP_P(result, MUL_P_Q(MUL_Pxk_P(p2, i), p1.koefP[i]));
 		}
@@ -174,7 +174,7 @@ t_Poli MOD_PP_P(t_Poli p1, t_Poli p2)
 	div = DIV_PP_P(p1, p2);
 	int_part = MUL_PP_P(div, p2);
 	quotient = SUB_PP_P(p1, int_part);
-	for (i = quotient.m; i > 0 && quotient.koefP[i].numerator.koef[p1.koefP[i].numerator.num - 1] == 0; i--)
+	for (i = quotient.m; i > 0 && quotient.koefP[i].Numer_Z[p1.koefP[i].Deg_Z - 1] == 0; i--)
 	{
 		quotient.m--;
 	}
@@ -190,7 +190,7 @@ t_Poli GCF_PP_P(t_Poli p1, t_Poli p2)
 		p1 = p2;
 		p2 = temp;
 	}
-	while (DEG_P_N(p2) != 0 || p2.koefP[0].numerator.koef[p2.koefP[0].numerator.num - 1] != 0)
+	while (DEG_P_N(p2) != 0 || p2.koefP[0].Numer_Z[p2.koefP[0].Dgr_Z - 1] != 0)
 	{
 		temp = p2;
 		p2 = MOD_PP_P(p1, p2);
@@ -203,13 +203,13 @@ t_Poli DER_P_P(t_Poli p)
 {
 	int i, j;
 	t_Rat I, ONE;
-	ONE.numerator.num = 1;
-	ONE.denominator.num = 1;
-	ONE.numerator.sgn = 0;
-	ONE.numerator.koef = malloc(4);
-	ONE.denominator.koef = malloc(4);
-	ONE.denominator.koef[0] = 1;
-	ONE.numerator.koef[0] = 1;
+	ONE.Dgr_Z = 1;
+	ONE.Dgr_N = 1;
+	ONE.Sgn_Z = 0;
+	ONE.Numer_Z = malloc(4);
+	ONE.Denom_N = malloc(4);
+	ONE.Numer_Z[0] = 1;
+	ONE.Denom_N[0] = 1;
 	I = SUB_QQ_Q(ONE, ONE);
 	t_Poli temp;
 	for (i = 0; i <= p.m; i++)
@@ -236,23 +236,23 @@ t_Poli NMR_P_P(t_Poli p, t_Poli r)
 	df = DER_P_P(p);
 	for (i = 0; i < r.m; i++)
 	{
-		sum.numerator.sgn = 0;
-		sum.numerator.koef = malloc(4);
-		sum.numerator.koef[0] = 0;
-		sum.numerator.num = 1;
-		sum.denominator.koef = malloc(4);
-		sum.denominator.koef[0] = 1;
-		sum.denominator.num = 1;
+		sum.Sgn_Z = 0;
+		sum.Numer_Z = malloc(4);
+		sum.Numer_Z[0] = 0;
+		sum.Dgr_Z = 1;
+		sum.Denom_N = malloc(4);
+		sum.Denom_N[0] = 1;
+		sum.Dgr_N = 1;
 		for (j = df.m; j >= 0; j--)
 		{
 			sum = ADD_QQ_Q(sum, df.koefP[j] * POWER_Q(r.koefP[i], j));
 		}
 		L.m = 1;
 		L.koefP = malloc(sizeof(t_Rat) * (L.m + 1));
-		r.koefP[i].numerator = MUL_ZM_Z(r.koefP[i].numerator);
+		r.koefP[i].Numer_Z = MUL_ZM_Z(r.koefP[i].Numer_Z);
 		L.koefP[0] = r.koefP[i];
 		L.koefP[1] = 1;
-		if (sum.numerator.koef[sum.numerator.num] == 0)
+		if (sum.Numer_Z[sum.Dgr_Z] == 0)
 		{
 			do
 			{
@@ -276,14 +276,14 @@ t_Rat POWER_Q(t_Rat q, int power)
 int COM_QQ_I(t_Rat q1, t_Rat q2)
 {
 	t_Int res1, res2;
-	if (q1.numerator.sgn > q2.numerator.sgn) return 1;//2nd is larger
-	if (q1.numerator.sgn < q2.numerator.sgn) return 2;//1st is larger
-	res1 = MUL_ZZ_Z(TRANS_N_Z(q2.denominator), q1.numerator);
-	res2 = MUL_ZZ_Z(TRANS_N_Z(q1.denominator), q2.numerator);
-	if (COM_NN_D(ABS_Z_N(res1), ABS_Z_N(res2)) == 2 && q1.numerator.sgn == 0) return 1;
-	elif(COM_NN_D(ABS_Z_N(res1), ABS_Z_N(res2)) == 2 && q1.numerator.sgn == 1) return 2;
-	elif(COM_NN_D(ABS_Z_N(res1), ABS_Z_N(res2)) == 1 && q1.numerator.sgn == 0) return 2;
-	elif(COM_NN_D(ABS_Z_N(res1), ABS_Z_N(res2)) == 1 && q1.numerator.sgn == 1) return 1;
+	if (q1.Sgn_Z > q2.Sgn_Z) return 1;//2nd is larger
+	if (q1.Sgn_Z < q2.Sgn_Z) return 2;//1st is larger
+	res1 = MUL_ZZ_Z(TRANS_N_Z(q2.Denom_N), q1.Numer_Z);
+	res2 = MUL_ZZ_Z(TRANS_N_Z(q1.Denom_N), q2.Numer_Z);
+	if (COM_NN_D(ABS_Z_N(res1), ABS_Z_N(res2)) == 2 && q1.Sgn_Z == 0) return 1;
+	elif(COM_NN_D(ABS_Z_N(res1), ABS_Z_N(res2)) == 2 && q1.Sgn_Z == 1) return 2;
+	elif(COM_NN_D(ABS_Z_N(res1), ABS_Z_N(res2)) == 1 && q1.Sgn_Z == 0) return 2;
+	elif(COM_NN_D(ABS_Z_N(res1), ABS_Z_N(res2)) == 1 && q1.Sgn_Z == 1) return 1;
 	elif(COM_NN_D(ABS_Z_N(res1), ABS_Z_N(res2)) == 0) return 0;
 }
 
