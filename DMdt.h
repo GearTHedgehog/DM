e03e0cb0d83fa46ce28a27bb78d05dd4c8974321
GearTHedgehog <danya400@yandex.ru>
2017-04-03 06:54:09 +0300

    Update DMdt.h

--- DMdt.h
+++ DMdt.h
@@ -1,21 +1,3 @@
-typedef struct Ratio
-{
-	int *Numer_Z;
-	int Dgr_Z;
-	int Sgn_Z;
-	
-	int *Denom_N;
-	int Dgr_N;
-	
-	/*
-	*Numer_Z - указатель на массив цифр целого числа дроби
-	DgrZ - номер старшей позиции целого числа дроби
-	SgnZ - знак целого числа дроби
-	*Denomr_N - указатель на массив цифр натурального числа дроби
-	DgrN - номер старшей позиции натурального числа дроби
-	*/
-} t_Rat;
-
 typedef struct Integer
 {
 	int *Numer_Z;
@@ -28,6 +10,19 @@ typedef struct Natural
 	int *Denom_N;
 	int Dgr_N;
 } t_Nat;
+
+typedef struct Ratio
+{
+	t_Int Numer;
+	t_Nat Denom;
+	/*
+	*Numer_Z - указатель на массив цифр целого числа дроби
+	DgrZ - номер старшей позиции целого числа дроби
+	SgnZ - знак целого числа дроби
+	*Denomr_N - указатель на массив цифр натурального числа дроби
+	DgrN - номер старшей позиции натурального числа дроби
+	*/
+} t_Rat;
 
 typedef struct Polinomial
 {
