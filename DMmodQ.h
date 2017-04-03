7c5c3ece369898e917e811c071e2ab332b151d65
GearTHedgehog <danya400@yandex.ru>
2017-04-03 06:52:58 +0300

    Update DMmodQ.h

--- DMmodQ.h
+++ DMmodQ.h
@@ -1,13 +1,5 @@
-// DM_Rational.cpp: определяет точку входа для консольного приложения.
-//
-
-#include <iostream>
-#include "conio.h"
-#include <C:\Users\Extranet\Desktop\DM_6305_KL-master\DMdt.h>
-using namespace std;
-
 //prototypes
-t_Rat RED_QQ_Q(t_Rat Q);
+t_Rat RED_Q_Q(t_Rat Q);
 int INT_Q_B(t_Rat Q);
 t_Rat TRANS_Z_Q(t_Int Z);
 t_Int TRANS_Q_Z(t_Rat Q);
@@ -21,38 +13,27 @@ t_Rat DIV_QQ_Q(t_Rat ftQ, t_Rat sdQ);
 Получает на вход рациональное число (адреса на ячейки памяти, хранящие первое рациональное число)
 GCD - НОД числителя и знаменателя дроби
 */
-t_Rat RED_QQ_Q(t_Rat Q)
+t_Rat RED_Q_Q(t_Rat Q)
 {
 	t_Nat GCD;
-	t_Int GCDforZ;
 	t_Nat N;
 	t_Int Z;
+	t_Rat Res;
 
 	//Разделение рационального числа на целое и натуральное
-	Z.Numer_Z = Q.Numer_Z;
-	Z.Dgr_Z = Q.Dgr_Z;
-	Z.Sgn_Z = Q.Sgn_Z;
-	N.Denom_N = Q.Denom_N;
-	N.Dgr_N = Q.Dgr_N;
+	Z = Q.Numer;
+	N = Q.Denom;
 
 	GCD = GCF_NN_N(ABS_Z_N(Z), N); //НОД числителя и знаменателя дроби
-
-	GCDforZ = TRANS_N_Z(GCD);
-
-	Z = DIV_ZZ_Z(Z, GCDforZ); //Деление числителя на НОД числителя и знаменателя
-
+	Z = DIV_ZZ_Z(Z, GCD); //Деление числителя на НОД числителя и знаменателя
 	//Запись числителя рационального числа
-	Q.Numer_Z = Z.Numer_Z;
-	Q.Dgr_Z = Z.Dgr_Z;
-	Q.Sgn_Z = Z.Sgn_Z;
-
+	Res.Numer = Z;
 	N = DIV_NN_N(N, GCD); //Деление знаменателя на НОД числителя и знаменателя
 
 	//Запись знаменателя рационального числа
-	Q.Denom_N = N.Denom_N;
-	Q.Dgr_N = N.Dgr_N;
+	Res.Denom = N;
 
-	return Q;
+	return Res;
 
 	//Алгоритм - Вера Степанова
 	//Код - Вера Степанова, Евгений Гонштейн(ред.)
@@ -62,30 +43,11 @@ t_Rat RED_QQ_Q(t_Rat Q)
 
 int INT_Q_B(t_Rat Q)
 {
-	t_Nat N, Rem;
-	t_Int Z;
+	Q = RED_Q_Q(Q);
 
-	Z.Numer_Z = Q.Numer_Z;
-	Z.Dgr_Z = Q.Dgr_Z;
-	Z.Sgn_Z = Q.Sgn_Z;
-	N.Denom_N = Q.Denom_N;
-	N.Dgr_N = Q.Dgr_N;
+	if (Q.Denom.Denom_N[0] == 1 && Q.Denom.Dgr_N == 0) return 1;
 
-	N= 2 ABS_Z_N(Z);
-
-	//Сравнение числителя и знаменателя
-	if ((COM_NN_D(N, ABS_Z_N(Z))) < 2)
-	{
-		Rem = MOD_NN_N(N, ABS_Z_N(Z)); //Вычисление остатка от деления числителя на знаменатель
-
-		//Проверка остатка на равность "0"
-		if (NZER_N_B(Rem))
-		{
-			return 1;
-		}
-	}
-
-	return 0;
+	return 0;	
 
 	//Алгоритм - Александр Силинский
 	//Код - Гонштейн Евгений
@@ -97,12 +59,10 @@ t_Rat TRANS_Z_Q(t_Int Z)
 {
 	t_Rat Q;
 
-	Q.Numer_Z = Z.Numer_Z;
-	Q.Dgr_Z = Z.Dgr_Z;
-	Q.Sgn_Z = Z.Sgn_Z;
-	Q.Denom_N = (int*)malloc(sizeof(int));
-	Q.Denom_N[0] = 1;
-	Q.Dgr_N = 0;
+	Q.Numer = Z;
+	Q.Denom.Denom_N = (int*)malloc(sizeof(int));
+	Q.Denom.Denom_N[0] = 1;
+	Q.Denom.Dgr_N = 0;
 
 	return Q;
 
@@ -116,82 +76,47 @@ t_Int TRANS_Q_Z(t_Rat Q)
 {
 	t_Int Z;
 
-	//Проверка знаменателя на равность "1"
-	if ((Q.Denom_N[0] == 1) && (Q.Dgr_N = 0))
-	{
-		Z.Numer_Z = Q.Numer_Z;
-		Z.Dgr_Z = Q.Dgr_Z;
-		Z.Sgn_Z = Q.Sgn_Z;
+	Z = Q.Numer;
 
-		return Z;
-	}
-	else return NULL;
+	return Z;
 
 	//Алгоритм - Гульназ Кинзябаева
 	//Код - Евгений Гонштейн
 	//Время - ~5 минут
 }
 
-/*
-fgEqual - флаг проверки равенства знаменателей при помощи модуля COM_NN_D
-*/
 t_Rat ADD_QQ_Q(t_Rat ftQ, t_Rat sdQ)
 {
-	int fgEqual;
 	t_Nat ftN, sdN, N;
-	t_Int ftZ, sdZ, Z1, Z2;
+	t_Int ftZ, sdZ, Z1, Z2, Z;
 	t_Rat Res;
 
 	//Разделение первого рационального числа на целое и натуральное
-	ftZ.Numer_Z = ftQ.Numer_Z;
-	ftZ.Dgr_Z = ftQ.Dgr_Z;
-	ftZ.Sgn_Z = ftQ.Sgn_Z;
-	ftN.Denom_N = ftQ.Denom_N;
-	ftN.Dgr_N = ftQ.Dgr_N;
+	ftZ = ftQ.Numer;
+	ftN = ftQ.Denom;
 
 	//Разделение второго рационального числа на целое и натуральное
-	sdZ.Numer_Z = sdQ.Numer_Z;
-	sdZ.Dgr_Z = sdQ.Dgr_Z;
-	sdZ.Sgn_Z = sdQ.Sgn_Z;
-	sdN.Denom_N = sdQ.Denom_N;
-	sdN.Dgr_N = sdQ.Dgr_N;
-
-	//fgEqual = COM_NN_D(ftN, sdN); //Проверка знаменателей первой и второй дробей на равенство
+	sdZ = sdQ.Numer;
+	sdN = sdQ.Denom;
 
 	if (COM_NN_D(ftN, sdN) != 0)
 	{//В случае, если знаменатели не равны, приводим дроби к общему знаменателю
-		sdZ.Numer_Z = sdQ.Denom_N;
-		sdZ.Dgr_Z = sdQ.Dgr_N;
-		sdZ.Sgn_Z = sdQ.Sgn_Z;
+		Z1 = MUL_ZZ_Z(ftZ, TRANS_N_Z(sdN)); //Умножение числителя первой дроби на знаменатель второй дроби
 
-		Z1 = MUL_ZZ_Z(ftZ, sdZ); //Умножение числителя первой дроби на знаменатель второй дроби
-
-
-		sdZ.Numer_Z = sdQ.Numer_Z;
-		sdZ.Dgr_Z = sdQ.Dgr_Z;
-		sdZ.Sgn_Z = sdQ.Sgn_Z;
-		ftZ.Numer_Z = sdQ.Denom_N;
-		ftZ.Dgr_Z = sdQ.Dgr_N;
-		sdZ.Sgn_Z = sdQ.Sgn_Z;
-
-		Z2 = MUL_ZZ_Z(sdZ, ftZ); //Умножение числителя второй дроби на знаменатель первой дроби
-
-
-		ftN.Denom_N = ftQ.Denom_N;
-		ftN.Dgr_N = ftQ.Dgr_N;
-		sdN.Denom_N = sdQ.Denom_N;
-		sdN.Dgr_N = sdQ.Dgr_N;
+		Z2 = MUL_ZZ_Z(sdZ, TRANS_N_Z(ftN)); //Умножение числителя второй дроби на знаменатель первой дроби
 
 		N = MUL_NN_N(ftN, sdN); //Умножение знаменателей двух дробей между собой, результат - общий знаменатель двух исходных дробей
 	}
+	else 
+	{
+		Z1 = ftZ;
+		Z2 = sdZ;
+		N = ftN;
+	}
 
-	Z1 = ADD_ZZ_Z(Z1, Z2); //Сложение числителей первой и второй дроби
-
-	Res.Numer_Z = Z1.Numer_Z;
-	Res.Dgr_Z = Z1.Dgr_Z;
-	Res.Sgn_Z = Z1.Sgn_Z;
-	Res.Denom_N = N.Denom_N;
-	Res.Dgr_N = N.Dgr_N;
+	Z = ADD_ZZ_Z(Z1, Z2); //Сложение числителей первой и второй дроби
+	Res.Numer = Z;
+	Res.Denom = N;
 
 	Res = RED_Q_Q(Res); //Сокращение полученной дроби
 
@@ -202,66 +127,33 @@ t_Rat ADD_QQ_Q(t_Rat ftQ, t_Rat sdQ)
 	//Время - 5-7 минут
 }
 
-/*
-fgEqual - флаг проверки равенства знаменателей при помощи модуля COM_NN_D
-*/
 t_Rat SUB_QQ_Q(t_Rat ftQ, t_Rat sdQ)
 {
-	int fgEqual;
 	t_Nat ftN, sdN, N;
-	t_Int ftZ, sdZ, Z1, Z2;
+	t_Int ftZ, sdZ, Z1, Z2, Z;
 	t_Rat Res;
 
 	//Разделение первого рационального числа на целое и натуральное
-	ftZ.Numer_Z = ftQ.Numer_Z;
-	ftZ.Dgr_Z = ftQ.Dgr_Z;
-	ftZ.Sgn_Z = ftQ.Sgn_Z;
-	ftN.Denom_N = ftQ.Denom_N;
-	ftN.Dgr_N = ftQ.Dgr_N;
+	ftZ = ftQ.Numer;
+	ftN = ftQ.Denom;
 
 	//Разделение второго рационального числа на целое и натуральное
-	sdZ.Numer_Z = sdQ.Numer_Z;
-	sdZ.Dgr_Z = sdQ.Dgr_Z;
-	sdZ.Sgn_Z = sdQ.Sgn_Z;
-	sdN.Denom_N = sdQ.Denom_N;
-	sdN.Dgr_N = sdQ.Dgr_N;
+	sdZ = sdQ.Numer;
+	sdN = sdQ.Denom;
 
-	fgEqual = COM_NN_D(ftN, sdN); //Проверка знаменателей первой и второй дробей на равенство
-
-	if (equal != 0)
+	if (COM_NN_D(ftN, sdN) != 0)
 	{//В случае, если знаменатели не равны, приводим дроби к общему знаменателю
-		sdZ.Numer_Z = sdQ.Denom_N;
-		sdZ.Dgr_Z = sdQ.Dgr_N;
-		sdZ.Sgn_Z = sdQ.Sgn_Z;
+		Z1 = MUL_ZZ_Z(ftZ, TRANS_N_Z(sdN)); //Умножение числителя первой дроби на знаменатель второй дроби
 
-		Z1 = MUL_ZZ_Z(ftZ, sdZ); //Умножение числителя первой дроби на знаменатель второй дроби
-
-
-		sdZ.Numer_Z = sdQ.Numer_Z;
-		sdZ.Dgr_Z = sdQ.Dgr_Z;
-		sdZ.Sgn_Z = sdQ.Sgn_Z;
-		ftZ.Numer_Z = sdQ.Denom_N;
-		ftZ.Dgr_Z = sdQ.Dgr_N;
-		sdZ.Sgn_Z = sdQ.Sgn_Z;
-
-		Z2 = MUL_ZZ_Z(sdZ, ftZ); //Умножение числителя второй дроби на знаменатель первой дроби
-
-
-		ftN.Denom_N = ftQ.Denom_N;
-		ftN.Dgr_N = ftQ.Dgr_N;
-		sdN.Denom_N = sdQ.Denom_N;
-		sdN.Dgr_N = sdQ.Dgr_N;
+		Z2 = MUL_ZZ_Z(sdZ, TRANS_N_Z(ftN)); //Умножение числителя второй дроби на знаменатель первой дроби
 
 		N = MUL_NN_N(ftN, sdN); //Умножение знаменателей двух дробей между собой, результат - общий знаменатель двух исходных дробей
 	}
 
-	Z1 = SUB_ZZ_Z(Z1, Z2); //Вычитание числителей первой и второй дроби
+	Z = SUB_ZZ_Z(Z1, Z2); //Вычитание числителей первой и второй дроби
 
-	Res.Numer_Z = Z1.Numer_Z;
-	Res.Dgr_Z = Z1.Dgr_Z;
-	Res.Sgn_Z = Z1.Sgn_Z;
-	Res.Denom_N = N.Denom_N;
-	Res.Dgr_N = N.Dgr_N;
+	Res.Numer = Z;
+	Res.Denom = N;
 
 	Res = RED_Q_Q(Res); //Сокращение полученной дроби
 
@@ -275,36 +167,27 @@ t_Rat SUB_QQ_Q(t_Rat ftQ, t_Rat sdQ)
 
 t_Rat MUL_QQ_Q(t_Rat ftQ, t_Rat sdQ)
 {
-	t_Nat ftN, sdN;
-	t_Int ftZ, sdZ;
+	t_Nat ftN, sdN, N;
+	t_Int ftZ, sdZ, Z;
 	t_Rat Res;
 
 	//Разделение первого рационального числа на целое и натуральное
-	ftZ.Numer_Z = ftQ.Numer_Z;
-	ftZ.Dgr_Z = ftQ.Dgr_Z;
-	ftZ.Sgn_Z = ftQ.Sgn_Z;
-	ftN.Denom_N = ftQ.Denom_N;
-	ftN.Dgr_N = ftQ.Dgr_N;
+	ftZ = ftQ.Numer;
+	ftN = ftQ.Denom;
 
 	//Разделение второго рационального числа на целое и натуральное
-	sdZ.Numer_Z = sdQ.Numer_Z;
-	sdZ.Dgr_Z = sdQ.Dgr_Z;
-	sdZ.Sgn_Z = sdQ.Sgn_Z;
-	sdN.Denom_N = sdQ.Denom_N;
-	sdN.Dgr_N = sdQ.Dgr_N;
+	sdZ = sdQ.Numer;
+	sdN = sdQ.Denom;
 
-	ftZ = MUL_ZZ_Z(ftZ, sdZ); //Умножение целых чисел (числителей)
+	Z = MUL_ZZ_Z(ftZ, sdZ); //Умножение целых чисел (числителей)
 
-							  //Запись числителя рационального числа
-	Res.Numer_Z = ftZ.Numer_Z;
-	Res.Dgr_Z = ftZ.Dgr_Z;
-	Res.Sgn_Z = ftZ.Sgn_Z;
+	//Запись числителя рационального числа
+	Res.Numer = Z;
 
-	ftN = MUL_NN_N(ftN, sdN); //Умножение натуральных чисел (знаменателей)
+	N = MUL_NN_N(ftN, sdN); //Умножение натуральных чисел (знаменателей)
 
-							  //Запись знаменателя рационального числа
-	Res.Denom_N = ftN.Denom_N;
-	Res.Dgr_N = ftN.Dgr_N;
+	//Запись знаменателя рационального числа
+	Res.Denom = N;
 
 	Res = RED_Q_Q(Res); //Сокращение дроби
 
@@ -318,36 +201,28 @@ t_Rat MUL_QQ_Q(t_Rat ftQ, t_Rat sdQ)
 
 t_Rat DIV_QQ_Q(t_Rat ftQ, t_Rat sdQ)
 {
-	t_Nat ftN, sdN;
-	t_Int ftZ, sdZ;
+	t_Nat ftN, sdN, N;
+	t_Int ftZ, sdZ, Z;
 	t_Rat Res;
 
 	//Разделение первого рационального числа на целое и натуральное
-	ftZ.Numer_Z = ftQ.Numer_Z;
-	ftZ.Dgr_Z = ftQ.Dgr_Z;
-	ftZ.Sgn_Z = ftQ.Sgn_Z;
-	ftN.Denom_N = ftQ.Denom_N;
-	ftN.Dgr_N = ftQ.Dgr_N;
+	ftZ = ftQ.Numer;
+	ftN = ftQ.Denom;
 
 	//Разделение второго рационального числа на целое и натуральное
-	sdZ.Numer_Z = sdQ.Denom_N;
-	sdZ.Dgr_Z = sdQ.Dgr_N;
-	sdZ.Sgn_Z = sdQ.Sgn_Z;
-	sdN.Denom_N = sdQ.Numer_Z;
-	sdN.Dgr_N = sdQ.Dgr_Z;
+	sdZ = TRANS_N_Z(sdQ.Denom);
+	sdZ.Sgn_Z = sdQ.Numer.Sgn_Z;
+	sdN = TRANS_Z_N(sdQ.Numer);
 
-	ftZ = MUL_ZZ_Z(ftZ, sdZ); //Умножение целых чисел (числителей)
+	Z = MUL_ZZ_Z(ftZ, sdZ); //Умножение целых чисел (числителей)
 
 	//Запись числителя рационального числа
-	Res.Numer_Z = ftZ.Numer_Z;
-	Res.Dgr_Z = ftZ.Dgr_Z;
-	Res.Sgn_Z = ftZ.Sgn_Z;
+	Res.Numer = Z;
 
-	ftN = MUL_NN_N(ftN, sdN); //Умножение натуральных чисел (знаменателей)
+	N = MUL_NN_N(ftN, sdN); //Умножение натуральных чисел (знаменателей)
 
 	//Запись знаменателя рационального числа
-	Res.Denom_N = ftN.Denom_N;
-	Res.Dgr_N = ftN.Dgr_N;
+	Res.Denom = N;
 
 	Res = RED_Q_Q(Res); //Сокращение дроби
 
