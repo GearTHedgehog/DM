cb299a9f951d38a4330db3fb1f9d189824bdb439
GearTHedgehog <danya400@yandex.ru>
2017-04-04 16:46:27 +0300

    Update DMmodP.h

--- DMmodP.h
+++ DMmodP.h
@@ -1,12 +1,54 @@
-#include <iostream>
-#include <C:\Users\Extranet\Desktop\DM_6305_KL-master\DMdt.h>
-#include <C:\Users\Extranet\Desktop\DM_6305_KL-master\gear.h>
-using namespace std;
+t_Rat POWER_Q(t_Rat, int);
+int COM_QQ_I(t_Rat, t_Rat);
+int COM_PP_I(t_Poli, t_Poli);
 
-//ñîáñòâåííûå ôóíêöèè, ðàçðàáîòàííûå äëÿ ïîääåðæàíèÿ ðàáîòû íåñêîëüêèõ äðóãèõ ôóíêöèé
-t_Rat POWER_Q(t_Rat, int)
-int COM_QQ_I(t_Rat, t_Rat)
-int COM_PP_I(t_Poli, t_Poli)
+/*t_Rat ZERO;
+	ZERO.Numer.Dgr_Z = 0;
+	ZERO.Denom.Dgr_N = 0;
+	ZERO.Numer.Sgn_Z = 0;
+	ZERO.Numer.Numer_Z = malloc(4);
+	ZERO.Denom.Denom_N = malloc(4);
+	ZERO.Numer.Numer_Z[0] = 0;
+	ZERO.Denom.Denom_N[0] = 1;*/
+	
+void outMnch1 (t_Poli polEx)
+{
+	int i, j;
+	puts ("THIS IS MNOGO4LEN");
+	for (i = polEx.m; i >= 0; i--)
+	{
+		if (polEx.koefP[i].Numer.Sgn_Z == 1) { putchar('-'); }
+		for (j = polEx.koefP[i].Numer.Dgr_Z; j >= 0; j--)
+		{
+			printf ("%d", polEx.koefP[i].Numer.Numer_Z[j]);
+		}
+		putchar ('/');
+		for (j = polEx.koefP[i].Denom.Dgr_N; j >= 0; j--)
+		{
+			printf ("%d", polEx.koefP[i].Denom.Denom_N[j]);
+		}
+		if (i != 0) { printf("*x^%d + ", i); }
+	}
+	putchar('\n');
+	printf("OUTMNCH OVER!\n");
+}
+
+void outRat1(t_Rat ratEx)
+{
+	int i;
+	if (ratEx.Numer.Sgn_Z == 1) { putchar('-'); }
+	for (i = ratEx.Numer.Dgr_Z; i >= 0; i--)
+	{
+		printf ("%d", ratEx.Numer.Numer_Z[i]);
+	}
+	putchar ('/');
+	for (i = ratEx.Denom.Dgr_N; i >= 0; i--)
+	{
+		printf ("%d", ratEx.Denom.Denom_N[i]);
+	}
+	putchar('\n');
+}
+
 
 t_Poli ADD_PP_P(t_Poli p1, t_Poli p2)
 {
@@ -14,27 +56,28 @@ t_Poli ADD_PP_P(t_Poli p1, t_Poli p2)
 	t_Poli result;
 	if (p1.m >= p2.m)
 	{
-		result.koefP = malloc(sizeof(t_Rat) * (p1.m + 1));
-		for (i = 0; i <= p1.m; i++)
+		result.koefP = (t_Rat*)malloc(sizeof(t_Rat) * (p1.m + 1));
+		for (i = 0; i <= p2.m; i++)
 		{
+			//printf("ADD_QQ_Q RES IN ADD_PP_P = "); outRat1(ADD_QQ_Q(p1.koefP[i], p2.koefP[i]));
 			result.koefP[i] = ADD_QQ_Q(p1.koefP[i], p2.koefP[i]);
+		}
+		for (j = i; j <= p1.m; j++)
+		{
+			result.koefP[j] = p1.koefP[j];
 		}
 		result.m = p1.m;
 	}
 	else
 	{
-		result.koefP = malloc(sizeof(t_Rat) * (p2.m + 1));
-		p1.koefP = realloc(p1.koefP, sizeof(t_Rat) * (p2.m + 1));
-		for (j = p1.m + 1; j <= p2.m; j++)
+		result.koefP = (t_Rat*) malloc (sizeof(t_Rat) * (p2.m + 1));
+		for (i = 0; i <= p1.m; i++)
 		{
-			for (i = p1.koefP[j].Dgr_Z; i >= 0; i--)
-			{
-				p1.koefP[j].NumerZ[i] = 0;
-			}
+			result.koefP[i] = ADD_QQ_Q(p2.koefP[i], p1.koefP[i]);
 		}
-		for (i = 0; i <= p2.m; i++)
+		for (j = i; j <= p2.m; j++)
 		{
-			result.koefP[i] = ADD_QQ_Q(p1.koefP[i], p2.koefP[i]);
+			result.koefP[j] = p2.koefP[j];
 		}
 		result.m = p2.m;
 	}
@@ -48,27 +91,27 @@ t_Poli SUB_PP_P(t_Poli p1, t_Poli p2)
 	if (p1.m >= p2.m)
 	{
 		result.m = p1.m;
-		result.koefP = malloc(sizeof(t_Rat) * (result.m + 1));
-		for (i = p1.m; i >= 0; i--)
+		result.koefP = (t_Rat*)malloc(sizeof(t_Rat) * (result.m + 1));
+		for (i = 0; i <= p2.m; i++)
 		{
 			result.koefP[i] = SUB_QQ_Q(p1.koefP[i], p2.koefP[i]);
+		}
+		for (j = i; j <= p1.m;j++)
+		{
+			result.koefP[j] = p1.koefP[j];
 		}
 	}
 	else
 	{
 		result.m = p2.m;
-		result.koefP = malloc(sizeof(t_Rat) * result.m);
-		p1.koefP = realloc(p1.koefP, sizeof(t_Rat)*(p2.m + 1));
-		for (j = p1.m + 1; j <= p2.m; j++)
+		result.koefP = (t_Rat*)malloc(sizeof(t_Rat) * result.m + 1);
+		for (i = 0; i <= p1.m; i++)
 		{
-			for (i = p1.koefP[j].Dgr_Z; i >= 0; i--)
-			{
-				p1.koefP[j].Numer_Z[i] = 0;
-			}
+			result.koefP[i] = SUB_QQ_Q(p2.koefP[i], p1.koefP[i]);
 		}
-		for (i = p2.m; i >= 0; i--)
+		for (j = i; j <= p2.m; j++)
 		{
-			result.koefP[i] = SUB_QQ_Q(p1.koefP[i], p2.koefP[i]);
+			result.koefP[j] = p2.koefP[j];
 		}
 	}
 	return result;
@@ -86,21 +129,31 @@ t_Poli MUL_PQ_P(t_Poli p, t_Rat q)
 
 t_Poli MUL_Pxk_P(t_Poli p, int xk)
 {
-	int i, j, k;
+	int i, j;
 	t_Poli result;
+	t_Rat ZERO;
+	ZERO.Numer.Dgr_Z = 0;
+	ZERO.Denom.Dgr_N = 0;
+	ZERO.Numer.Sgn_Z = 0;
+	ZERO.Numer.Numer_Z = malloc(4);
+	ZERO.Denom.Denom_N = malloc(4);
+	ZERO.Numer.Numer_Z[0] = 0;
+	ZERO.Denom.Denom_N[0] = 1;
 	result.m = p.m + xk;
-	result.koefP = malloc(sizeof(t_Rat) * (result.m + 1));
-	for (j = 0; j <= p.m; j++)
+	result.koefP = (t_Rat*)malloc(sizeof(t_Rat) * (result.m + 1));
+	for (i = 0; i < xk; i++)
 	{
-		for (i = p.koefP[j].Dgr_Z; i >= 0; i--)
+		result.koefP[i] = ZERO;
+		/*for (i = p.koefP[j].Numer.Dgr_Z; i >= 0; i--)
 		{
-			result.koefP[j].Numer_Z[i] = 0;
-		}
+			result.koefP[j].Numer.Numer_Z[i] = 0;
+		}*/
 	}
-	for (i = 0; i <= result.m; i++, j++)
+	for (i = xk, j = 0; i <= result.m; i++, j++)
 	{
-		result.koefP[j] = p.koefP[i];
+		result.koefP[i] = p.koefP[j];
 	}
+
 	return result;
 }
 
@@ -119,33 +172,45 @@ t_Rat FAC_P_Q(t_Poli p)
 	int i;
 	t_Rat result;
 	t_Nat tmpGCF;
-	t_Nat tmpLCM;
-	tmpGCF = TRANS_Z_N(ABS_Z_N(p.koefP[0].Numer_Z));
+	t_Nat tmpLCM;;
+	tmpGCF = ABS_Z_N(p.koefP[0].Numer);
 	for (i = 1; i < p.m; i++)
 	{
-		tmpGCF = GCF_NN_N(tmpGCF, TRANS_Z_N(ABS_Z_N(p.koefP[i].Numer_Z)));
+		tmpGCF = GCF_NN_N(tmpGCF, ABS_Z_N(p.koefP[i].Numer));
 	}
-	tmpLCM = p.koefP[0].Denom_N;
+	tmpLCM = p.koefP[0].Denom;
 	for (i = 1; i < p.m; i++)
 	{
-		tmpLCM = LCM_NN_N(tmpLCM, p.koefP[i].Denom_N);
+		tmpLCM = LCM_NN_N(tmpLCM, p.koefP[i].Denom);
 	}
-	result.Numer_Z = TRANS_N_Z(tmpGCF);
-	result.Denom_N = tmpLCM;
+	result.Numer = TRANS_N_Z(tmpGCF);
+	result.Denom = tmpLCM;
 	return result;
 }
 
 t_Poli MUL_PP_P(t_Poli p1, t_Poli p2)
 {
-	int i;
+	int i, j, k;
 	t_Poli result;
+	t_Rat ZERO;
+	ZERO.Numer.Dgr_Z = 0;
+	ZERO.Denom.Dgr_N = 0;
+	ZERO.Numer.Sgn_Z = 0;
+	ZERO.Numer.Numer_Z = malloc(4);
+	ZERO.Denom.Denom_N = malloc(4);
+	ZERO.Numer.Numer_Z[0] = 0;
+	ZERO.Denom.Denom_N[0] = 1;
 	result.m = p1.m + p2.m;
-	result.koefP = malloc(result.m + 1);
+	result.koefP = malloc((result.m + 1) * sizeof(t_Rat));
+	for (i = 0; i <= result.m; i++)
+	{
+		result.koefP[i] = ZERO;
+	}
 	for (i = 0; i <= p1.m; i++)
 	{
-		if (p1.koefP[i].Numer_Z[p1.koefP[i].Dgr_Z] != 0)
+		if (p1.koefP[i].Numer.Numer_Z[p1.koefP[i].Numer.Dgr_Z] != 0)
 		{
-			result = ADD_PP_P(result, MUL_P_Q(MUL_Pxk_P(p2, i), p1.koefP[i]));
+			result = ADD_PP_P(result, MUL_PQ_P(MUL_Pxk_P(p2, i), p1.koefP[i]));
 		}
 	}
 	return result;
@@ -153,17 +218,36 @@ t_Poli MUL_PP_P(t_Poli p1, t_Poli p2)
 
 t_Poli DIV_PP_P(t_Poli p1, t_Poli p2)
 {
-	int i, st, s;
+	int i, st, s, j;
 	s = p1.m;
 	t_Poli result;
+	t_Rat ZERO;
+	ZERO.Numer.Dgr_Z = 0;
+	ZERO.Denom.Dgr_N = 0;
+	ZERO.Numer.Sgn_Z = 0;
+	ZERO.Numer.Numer_Z = malloc(4);
+	ZERO.Denom.Denom_N = malloc(4);
+	ZERO.Numer.Numer_Z[0] = 0;
+	ZERO.Denom.Denom_N[0] = 1;
 	result.m = p1.m - p2.m;
-	result.koefP = malloc(sizeof(t_Rat) * (result.m + 1));
+	result.koefP = (t_Rat*)malloc(sizeof(t_Rat) * (result.m + 1));
+	
+	for (i = 0; i <= result.m; i++)
+	{
+		result.koefP[i] = ZERO;
+	}
+
 	for (i = 0, st = p1.m; st >= p2.m; i++)
 	{
 		result.koefP[s - p2.m - i] = DIV_QQ_Q(p1.koefP[s - i], p2.koefP[p2.m]);
-		p1 = SUB_PP_P(p1, MUL_P_Q(MUL_Pxk_P(p2, DEG_P_N(p1) - p2.m), result.koefP[s - p2.m - i]));
+		p1 = SUB_PP_P(p1, MUL_PQ_P(MUL_Pxk_P(p2, DEG_P_N(p1) - p2.m), result.koefP[s - p2.m - i]));
+		for (j = st; j >= 0; j--)
+		{
+			if (p1.koefP[j].Numer.Numer_Z[p1.koefP[i].Numer.Dgr_Z] == 0) { p1.m--; } else break;
+		}
 		st = DEG_P_N(p1);
 	}
+
 	return result;
 }
 
@@ -174,7 +258,7 @@ t_Poli MOD_PP_P(t_Poli p1, t_Poli p2)
 	div = DIV_PP_P(p1, p2);
 	int_part = MUL_PP_P(div, p2);
 	quotient = SUB_PP_P(p1, int_part);
-	for (i = quotient.m; i > 0 && quotient.koefP[i].Numer_Z[p1.koefP[i].Deg_Z - 1] == 0; i--)
+	for (i = quotient.m; i > 0 && quotient.koefP[i].Numer.Numer_Z[p1.koefP[i].Numer.Dgr_Z] == 0; i--)
 	{
 		quotient.m--;
 	}
@@ -190,7 +274,7 @@ t_Poli GCF_PP_P(t_Poli p1, t_Poli p2)
 		p1 = p2;
 		p2 = temp;
 	}
-	while (DEG_P_N(p2) != 0 || p2.koefP[0].Numer_Z[p2.koefP[0].Dgr_Z - 1] != 0)
+	while (DEG_P_N(p2) != 0 || p2.koefP[0].Numer.Numer_Z[p2.koefP[0].Numer.Dgr_Z - 1] != 0)
 	{
 		temp = p2;
 		p2 = MOD_PP_P(p1, p2);
@@ -202,62 +286,83 @@ t_Poli GCF_PP_P(t_Poli p1, t_Poli p2)
 t_Poli DER_P_P(t_Poli p)
 {
 	int i, j;
-	t_Rat I, ONE;
-	ONE.Dgr_Z = 1;
-	ONE.Dgr_N = 1;
-	ONE.Sgn_Z = 0;
-	ONE.Numer_Z = malloc(4);
-	ONE.Denom_N = malloc(4);
-	ONE.Numer_Z[0] = 1;
-	ONE.Denom_N[0] = 1;
-	I = SUB_QQ_Q(ONE, ONE);
+	t_Rat I, ONE, ZERO;
 	t_Poli temp;
-	for (i = 0; i <= p.m; i++)
+	temp.m = p.m;
+	temp.koefP = (t_Rat*)malloc(sizeof(t_Rat) * (temp.m + 1));
+	for (i = 0; i <= temp.m; i++)
 	{
-		p.koefP[i] = MUL_QQ_Q(p.koefP[i], I);
+		temp.koefP[i] = p.koefP[i];
+	}
+	ZERO.Numer.Dgr_Z = 0;
+	ZERO.Denom.Dgr_N = 0;
+	ZERO.Numer.Sgn_Z = 0;
+	ZERO.Numer.Numer_Z = malloc(4);
+	ZERO.Denom.Denom_N = malloc(4);
+	ZERO.Numer.Numer_Z[0] = 0;
+	ZERO.Denom.Denom_N[0] = 1;
+	ONE.Numer.Dgr_Z = 0;
+	ONE.Denom.Dgr_N = 0;
+	ONE.Numer.Sgn_Z = 0;
+	ONE.Numer.Numer_Z = malloc(4);
+	ONE.Denom.Denom_N = malloc(4);
+	ONE.Numer.Numer_Z[0] = 1;
+	ONE.Denom.Denom_N[0] = 1;
+	I = ONE;
+	for (i = 1; i <= temp.m; i++)
+	{
+		temp.koefP[i - 1] = MUL_QQ_Q(temp.koefP[i], I);
 		I = ADD_QQ_Q(I, ONE);
 	}
-	temp = p;
-	p.m--;
-	p.koefP = realloc(p.koefP, sizeof(t_Rat) * (p.m + 1));
-	for (i = p.m; i >= 0; i--)
-	{
-		p.koefP[i] = temp.koefP[temp.m--];
-	}
-	free(temp.koefP);
-	return p;
+	temp.m--;
+	temp.koefP = (t_Rat*)realloc(temp.koefP, sizeof(t_Rat) * (temp.m + 1));
+	return temp;
 }
 
 t_Poli NMR_P_P(t_Poli p, t_Poli r)
 {
 	int i, j;
 	t_Poli df, L;
-	t_Rat sum;
-	df = DER_P_P(p);
-	for (i = 0; i < r.m; i++)
+	t_Rat sum, ONE, ZERO;
+	t_Poli temp;
+	temp.m = p.m;
+	temp.koefP = (t_Rat*)malloc(sizeof(t_Rat) * (temp.m + 1));
+	for (i = 0; i <= temp.m; i++)
 	{
-		sum.Sgn_Z = 0;
-		sum.Numer_Z = malloc(4);
-		sum.Numer_Z[0] = 0;
-		sum.Dgr_Z = 1;
-		sum.Denom_N = malloc(4);
-		sum.Denom_N[0] = 1;
-		sum.Dgr_N = 1;
+		temp.koefP[i] = p.koefP[i];
+	}
+	ONE.Numer.Dgr_Z = 0;
+	ONE.Denom.Dgr_N = 0;
+	ONE.Numer.Sgn_Z = 0;
+	ONE.Numer.Numer_Z = malloc(4);
+	ONE.Denom.Denom_N = malloc(4);
+	ONE.Numer.Numer_Z[0] = 1;
+	ONE.Denom.Denom_N[0] = 1;
+	df = DER_P_P(temp);
+	for (i = 0; i <= r.m; i++)
+	{
+		sum.Numer.Sgn_Z = 0;
+		sum.Numer.Numer_Z = malloc(4);
+		sum.Numer.Numer_Z[0] = 0;
+		sum.Numer.Dgr_Z = 0;
+		sum.Denom.Denom_N = malloc(4);
+		sum.Denom.Denom_N[0] = 1;
+		sum.Denom.Dgr_N = 0;
 		for (j = df.m; j >= 0; j--)
 		{
-			sum = ADD_QQ_Q(sum, df.koefP[j] * POWER_Q(r.koefP[i], j));
+			sum = ADD_QQ_Q(sum, MUL_QQ_Q(df.koefP[j], POWER_Q(r.koefP[i], j)));
 		}
 		L.m = 1;
-		L.koefP = malloc(sizeof(t_Rat) * (L.m + 1));
-		r.koefP[i].Numer_Z = MUL_ZM_Z(r.koefP[i].Numer_Z);
+		L.koefP = (t_Rat*)malloc(sizeof(t_Rat) * (L.m + 1));
+		r.koefP[i].Numer = MUL_ZM_Z(r.koefP[i].Numer);
 		L.koefP[0] = r.koefP[i];
-		L.koefP[1] = 1;
-		if (sum.Numer_Z[sum.Dgr_Z] == 0)
+		L.koefP[1] = ONE;
+		if (sum.Numer.Numer_Z[sum.Numer.Dgr_Z] == 0)
 		{
 			do
 			{
-				p = DIV_PP_P(p, L);
-			} while (!COM_PP_I(GCF_PP_P(p, L), L));
+				temp = DIV_PP_P(temp, L);
+			} while (!COM_PP_I(GCF_PP_P(temp, L), L));
 		}
 	}
 	return p;
@@ -276,14 +381,14 @@ t_Rat POWER_Q(t_Rat q, int power)
 int COM_QQ_I(t_Rat q1, t_Rat q2)
 {
 	t_Int res1, res2;
-	if (q1.Sgn_Z > q2.Sgn_Z) return 1;//2nd is larger
-	if (q1.Sgn_Z < q2.Sgn_Z) return 2;//1st is larger
-	res1 = MUL_ZZ_Z(TRANS_N_Z(q2.Denom_N), q1.Numer_Z);
-	res2 = MUL_ZZ_Z(TRANS_N_Z(q1.Denom_N), q2.Numer_Z);
-	if (COM_NN_D(ABS_Z_N(res1), ABS_Z_N(res2)) == 2 && q1.Sgn_Z == 0) return 1;
-	elif(COM_NN_D(ABS_Z_N(res1), ABS_Z_N(res2)) == 2 && q1.Sgn_Z == 1) return 2;
-	elif(COM_NN_D(ABS_Z_N(res1), ABS_Z_N(res2)) == 1 && q1.Sgn_Z == 0) return 2;
-	elif(COM_NN_D(ABS_Z_N(res1), ABS_Z_N(res2)) == 1 && q1.Sgn_Z == 1) return 1;
+	if (q1.Numer.Sgn_Z > q2.Numer.Sgn_Z) return 1;//2nd is larger
+	if (q1.Numer.Sgn_Z < q2.Numer.Sgn_Z) return 2;//1st is larger
+	res1 = MUL_ZZ_Z(TRANS_N_Z(q2.Denom), q1.Numer);
+	res2 = MUL_ZZ_Z(TRANS_N_Z(q1.Denom), q2.Numer);
+	if (COM_NN_D(ABS_Z_N(res1), ABS_Z_N(res2)) == 2 && q1.Numer.Sgn_Z == 0) return 1;
+	elif(COM_NN_D(ABS_Z_N(res1), ABS_Z_N(res2)) == 2 && q1.Numer.Sgn_Z == 1) return 2;
+	elif(COM_NN_D(ABS_Z_N(res1), ABS_Z_N(res2)) == 1 && q1.Numer.Sgn_Z == 0) return 2;
+	elif(COM_NN_D(ABS_Z_N(res1), ABS_Z_N(res2)) == 1 && q1.Numer.Sgn_Z == 1) return 1;
 	elif(COM_NN_D(ABS_Z_N(res1), ABS_Z_N(res2)) == 0) return 0;
 }
 
